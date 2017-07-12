0afa63c174f599582d8cd4b7c99f11728abd78de
hannahreedy <hannahreedy@gmail.com>
2017-07-11 20:17:14 -0500

    Added first JS

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+window.onload = function(event) {
+  console.log('Hi Mom');
+  console.log(document);
+  let ul = document.createElement('ul');
+  console.log(ul);
+  document.getElementById('list').innerHTML = 'i like cats'
+}
